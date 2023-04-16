971954bb86d0375d289e868a1355e9a9aa0788e2
David Shannon <dzshanno@yahoo.com>
2023-04-16 12:34:22 +0100

    updated for 5 fingers

--- peripheralGlovepart2/peripheralGlovepart2.ino
+++ peripheralGlovepart2/peripheralGlovepart2.ino
@@ -12,10 +12,10 @@
 // Set the input pins the flex sensors are connected to
 
 int Flex1_pin = A0;
-int Flex2_pin = 0;
-int Flex3_pin = 0;
-int Flex4_pin = 0;
-int Flex5_pin = 0;
+int Flex2_pin = A1;
+int Flex3_pin = A2;
+int Flex4_pin = A3;
+int Flex5_pin = A4;
 
 // set the UUID of the BLE service
 BLEService FlexService("19B10000-E8F2-537E-4F6C-D104768A1214");
@@ -24,22 +24,26 @@ BLEService FlexService("19B10000-E8F2-537E-4F6C-D104768A1214");
 
 BLEByteCharacteristic Flex1("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
 BLEByteCharacteristic Flex2("19B10002-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-
-
-
+BLEByteCharacteristic Flex3("19B10003-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLEByteCharacteristic Flex4("19B10004-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLEByteCharacteristic Flex5("19B10005-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
 // run the setup actions
 
-void setup() {
+void setup()
+{
 
   // initiate serial connection for feedback information
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   // begin BLE initialization
-  if (!BLE.begin()) {
+  if (!BLE.begin())
+  {
     Serial.println("starting Bluetooth® Low Energy module failed!");
 
-    while (1);
+    while (1)
+      ;
   }
 
   // set advertised local name and service UUID:
@@ -49,6 +53,9 @@ void setup() {
   // add the characteristic to the service
   FlexService.addCharacteristic(Flex1);
   FlexService.addCharacteristic(Flex2);
+  FlexService.addCharacteristic(Flex3);
+  FlexService.addCharacteristic(Flex4);
+  FlexService.addCharacteristic(Flex5);
 
   // add service
   BLE.addService(FlexService);
@@ -56,6 +63,9 @@ void setup() {
   // set the initial value for the characeristic:
   Flex1.writeValue(0);
   Flex2.writeValue(0);
+  Flex3.writeValue(0);
+  Flex4.writeValue(0);
+  Flex5.writeValue(0);
   // start advertising
   BLE.advertise();
 
@@ -63,35 +73,27 @@ void setup() {
   Serial.println("Glove");
 }
 
-void loop() {
+void loop()
+{
   // listen for Bluetooth® Low Energy peripherals to connect:
   BLEDevice central = BLE.central();
 
   // if a central is connected to peripheral:
-  if (central) {
+  if (central)
+  {
     Serial.print("Connected to central: ");
     // print the central's MAC address:
     Serial.println(central.address());
 
     // while the central is still connected to peripheral:
-  while (central.connected()) {
-    int Flex1value = analogRead(Flex1_pin);
-    // BLE can only take a value up to 255 so check if its too high
-    if (Flex1value > 255) {
-      Serial.print("SPIKE - ");
-      Serial.print(Flex1value);
-      Flex1value = 255;
+    while (central.connected())
+    {
+      sendflexvalue(Flex1_pin, Flex1, "Flex1");
+      sendflexvalue(Flex2_pin, Flex2, "Flex2");
+      sendflexvalue(Flex3_pin, Flex3, "Flex3");
+      sendflexvalue(Flex4_pin, Flex4, "Flex4");
+      sendflexvalue(Flex5_pin, Flex5, "Flex pinky");
     }
-    // write the flex sensort value to the BLE characteristic
-    Flex1.writeValue(Flex1value);
-    Serial.print("Flex1 Analog value = ");
-    Serial.println(Flex1value);
-        if (Flex1.written()) {
-            Serial.print("BLE Flex1 value");
-            Serial.println(Flex1.value());      
-        }
-      delay(10);
-      }
 
     // when the central disconnects, print it out:
     Serial.print(F("Disconnected from central: "));
@@ -101,4 +103,21 @@ void loop() {
 }
 
 // Other functions called by the setup and loop
+void sendflexvalue(int pin, BLEByteCharacteristic characteristic, String name)
+{
 
+  int Flexvalue = analogRead(pin);
+  // BLE can only take a value up to 255 so check if its too high
+  if (Flexvalue > 255)
+  {
+    Serial.print("SPIKE - ");
+    Serial.print(Flexvalue);
+    Flexvalue = 255;
+  }
+  // write the flex sensort value to the BLE characteristic
+  characteristic.writeValue(Flexvalue);
+  Serial.print(name);
+  Serial.print(" Value is ...");
+  Serial.println(Flexvalue);
+  delay(10);
+}
